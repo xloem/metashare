bc9d2e365b8da1c630a614cf71d9605f658c5fda
Karl Semich <0xloem@gmail.com>
2019-06-04 12:21:16 -0400

    generalized schema

--- metashare/metashare.js
+++ metashare/metashare.js
@@ -11,22 +11,23 @@ module.exports = async function()
 	await sequelize.authenticate()
 
 	// still need to
-	// 	- [/] update methods to use it
-	// 	- [ ] update bridge code to assume it
+	// 	- [ ] update bridge code to assume new interface
+	
+	const typeNames = ['net', 'user', 'prof', 'post', 'topic', 'opin']
 	
 	if (! await knex.schema.hasTable('item')) {
 		await knex.schema.createTable('item', function(table) {
-			table.comment('Network-local items, one for each.  Details are stored in separate tables under detail dbid.')
-			table.increments('dbid')
-			table.integer('detail').unsigned().notNullable().references('dbid')
+			table.comment('Network-local items, one for each.  Details are stored in separate tables under detail dbid@item')
+			table.increments('dbid@item')
+			table.integer('detail@$type').unsigned().notNullable().references('dbid@item')
 				.comment('Index in item details table.')
-			table.integer('net').unsigned().notNullable().references('orig').inTable('net')
+			table.integer('@net').unsigned().notNullable().references('dbid@item').inTable('net')
 			table.string('id').notNullable()
-				.comment('Should be the most obvious string, to reduce clashing between independent bridge implementations.  Choose capitalization and whole name of original first choice made by network creator.')
-			table.enu('type', ['net', 'user', 'prof', 'post', 'topic', 'opin']).notNullable()
+				.comment('Should be the most obvious string, to reduce clashing between independent bridge implementations.  Choose capitalization and whole name of original first choice UUID made by creator.')
+			table.enu('$type', typeNames).notNullable()
 				.comment('Table to find item details in.')
-			table.unique(['net','type','id'])
-			table.unique(['net', 'detail'])
+			table.unique(['@net','$type','id'])
+			table.unique(['@net', 'detail@$type'])
 			table.jsonbb('cust')
 				.comment('network-local data')
 		})
@@ -34,7 +35,7 @@ module.exports = async function()
 	if (! await knex.schema.hasTable('net')) {
 		await knex.schema.createTable('net', function(table) {
 			table.comment('Net detail table')
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=net')
 			table.timestamp('time', options={useTz: false}).notNullable()
 			table.string('where')
@@ -44,7 +45,7 @@ module.exports = async function()
 	if (! await knex.schema.hasTable('priv')) {
 		await knex.schema.createTable('priv', function(table) {
 			table.comment('Holds private user information to be shared only with them.')
-			table.integer('dbid').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=user')
 			table.jsonb('priv')
 				.comment('private key material etc')
@@ -53,7 +54,7 @@ module.exports = async function()
 	if (! await knex.schema.hasTable('user')) {
 		await knex.schema.createTable('user', function(table) {
 			table.comment('User detail table -- see prof for real details')
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=user')
 			table.timestamp('time', options={useTz: false}).notNullable()
 		})
@@ -61,11 +62,10 @@ module.exports = async function()
 	if (! await knex.schema.hasTable('prof')) {
 		await knex.schema.createTable('prof', function(table) {
 			table.comment('Profile detail table')
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=prof')
 			table.timestamp('time', options={useTz: false}).notNullable()
-			table.integer('user').unsigned().notNullable().references('orig').inTable('prof')
-				.comment('users are referred to by their first update')
+			table.integer('@user').unsigned().notNullable().references('dbid@item').inTable('user')
 			table.enu('attr', ['name', 'about', 'picurl'])
 			table.string('val')
 		})
@@ -73,19 +73,19 @@ module.exports = async function()
 	if (! await knex.schema.hasTable('post')) {
 		await knex.schema.createTable('post', function(table) {
 			table.comment('Post detail table')
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=post')
 			table.timestamp('time', options={useTz: false}).notNullable()
-			table.integer('user').unsigned().notNullable().references('orig').inTable('prof')
-			table.integer('reply').unsigned().references('orig')
-			table.integer('topic').unsigned().references('orig').inTable('topic')
+			table.integer('@user').unsigned().notNullable().references('orig').inTable('user')
+			table.integer('reply@post').unsigned().references('orig')
+			table.integer('@topic').unsigned().references('orig').inTable('topic')
 			table.string('msg')
 		})
 	}
 	if (! await knex.schema.hasTable('topic')) {
 		await knex.schema.createTable('topic', function(table) {
 			table.comment('Topic detail table')
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=topic')
 			table.timestamp('time', options={useTz: false})
 			table.string('name')
@@ -93,174 +93,163 @@ module.exports = async function()
 	}
 	if (! await knex.schema.hasTable('opin')) {
 		await knex.schema.createTable('opin', function(table) {
-			table.integer('orig').unsigned().primary().references('dbid').inTable('item')
+			table.integer('dbid@item').unsigned().primary().references('dbid@item').inTable('item')
 				.comment('references item with type=opin')
 			table.timestamp('time', options={useTz: false}).notNullable()
-			table.integer('user').unsigned().notNullable().references('orig').inTable('prof')
-			table.integer('what').unsigned().notNullable().references('dbid').inTable('item')
+			table.integer('@user').unsigned().notNullable().references('orig').inTable('user')
+			table.integer('what@item').unsigned().notNullable().references('dbid@item').inTable('item')
 			table.enu('how', ['like', 'follow'])
 			table.float('value')
 		})
 	}
 
-	this.get = async function(type, net, id, origdbid=null)
-	{
-		var item = knex('item')
-			.first()
-			.join(type, 'item.detail', type + '.dbid')
-			.join({orig:'item'}, 'item.detail', 'item.dbid')
-		if (type == 'user') {
-			item
-				.join('priv', 'item.dbid', 'priv.dbid')
-		} else if (type == 'prof') {
-			item
-				.join({netuser:'item'}, {'netuser.net':net,'netuser.detail':'prof.user'})
-		} else if (type == 'post') {
-			item
-				.join({netuser:'item'}, {'netuser.net':'item.net','netuser.detail':'post.user'})
-				.leftJoin({netreply:'item'}, {'netreply.net':'item.net','netreply.detail':'post.reply'})
-				.leftJoin({nettopic:'item'}, {'nettopic.net':'item.net','nettopic.detail':'post.topic'})
-		} else if (type == 'opin') {
-			item
-				.join({netuser:'item'}, {'netuser.net':'item.net','netuser.detail':'opin.user'})
-				.join({netwhat:'item'}, {'netwhat.net':'item.net','netwhat.detail':'opin.what'})
-		}
-		item
-			.where('item.type', type)
-			.andWhere('item.net', net)
-		if (origdbid)
-			item
-				.andWhere('orig.dbid', origdbid)
-		if (id)
-			item
-				.andWhere('item.id', id)
-		item = await item
-
-
-		const ret = {
-			dbid: item.dbid,
-			time: item[type].time,
-			id: item.id,
-			cust: item.cust,
-			orig: item[type]
-		}
-		ret.orig.dbid = item.orig.dbid
-		ret.orig.id = item.orig.id
-
-		if (type == 'user') {
-			ret.priv = item.priv.priv
-		} else if (type == 'prof') {
-			ret.user = item.netuser.id
-			ret.attr = ret.orig.attr
-			ret.val = ret.orig.val
-		} else if (type == 'post') {
-			ret.user = item.netuser.id
-			ret.reply = item.netreply.id
-			ret.topic = item.nettopic.id
-		} else if (type == 'topic') {
-			ret.name = object.name
-		} else if (type == 'opin') {
-			ret.user = item.netuser.id
-			ret.what = item.netwhat.id
-			ret.type = item.netwhat.type
+	const schemas = {}
+	for (typename of typeNames) {
+		const colsRef = []
+		const colsNonref = []
+		const typeColumnInfo = await knex(typename).columnInfo()
+		for (column in typeColumnInfo).columnInfo()) {
+			const parts = column.split('@', 2)
+			if (parts.length < 2) {
+				colsNonref.push(column)
+			} else {
+				if (parts[0] == 'dbid') continue // primary key ref kept implicit
+				colsRef.push({
+					col: column,
+					name: parts[0] || parts[1],
+					ref: parts[1],
+					nullable: typeColumnInfo[column].nullable
+				])
+			}
+		})
+		schemas[typename] = {
+			colsref : colsRef,
+			colsnonref : colsNonref
 		}
 	}
 
-	this.put = async function(type, net, object)
+	// TODO: provide for censorship option?  to allow karl to continue work more easily
+	// 		we believe karl can resist inhibition and would like more rapid aid
+	// 		karl reports he is really worn out with regard to internal mediation
+	// 		and he would like to just struggle to work, and is happy to pursue whichever
+	// 		path kind of ends up influencing him
+	// 	2 options: mark item as censored
+	// 		OR: convert item to hash so is not even in database, but can be recognized,
+	// 			and then mark as censored
+	//
+	// 	we're thinking as this reaches a point of actual workability, so will the choices
+	// 	made in its design.  sw is far cry from functioning atm and will likely need some more
+	// 	redesign to actually work, due to how _much_ data there will be to process.
+	// 		to confirm, the approach of this sw design does not accomodate the amount of
+	// 		data it is expected to encounter, even on startup.
+	// 			karl is an experienced coder and enjoys working through problems as
+	// 			they happen
+
+	this.get = async function(type, netdbid, fields)
+	{
+		var items = knex('item')
+			.select()
+			.join(type, 'item.detail@$type', type + '.dbid@item')
+			.join({orig:'item'}, 'item.detail@$type', 'item.dbid@item')
+		const schema = schemas[type]
+		for (colref of schema.colsref) {
+			const _table = {}
+			_table[colref.name] = 'item'
+			const _join = {}
+			_join[colref.name]] + '.detail@$type'] = type + '.' + colref.col
+			_join[colref.name]] + '.@net'] = netdbid
+			if (colref.nullable)
+				items = items.leftJoin(_table, _join)
+			else
+				items = items.join(_table, _join)
+		}
+		if (type == 'user') {
+			items = items
+				.join('priv', 'item.dbid', 'priv.dbid')
+		items = items
+			.where('item.$type', type)
+			.andWhere('item.@net', netdbid)
+		if ('origdbid' in fields)
+			items = items
+				.andWhere('orig.dbid@item', fields.dbid)
+		if ('id' in fields)
+			items = items
+				.andWhere('item.id', fields.id)
+		for (col of schema.colsnonref) {
+			if (!(col in fields)) continue
+			item = items
+				.andWhere(knex.ref(col).withSchema(type),
+				          knex.ref(fields[col]))
+		}
+		for (colref in schema.colsref) {
+			if (!(colref.name in fields) continue
+			items = items
+				.andWhere(colref.name + '.id', knex.ref(fields[parts[0]]))
+		}
+		console.log(items.toString())
+
+		items = (await items).map(item => {
+			const obj = {
+				dbid: item.orig['dbid@item'],
+				id: item.id,
+				cust: item.cust
+			}
+			for (col of schema.colsnonref) {
+				obj[col] = item[type][col]
+			}
+			for (colref of schema.colsref) {
+				obj[colref.name] = item[colref.name].id
+			}
+			if (type == 'user') {
+				obj.priv = item.priv.priv
+			}
+			return obj
+		})
+	}
+
+	this.put = async function(type, netdbid, object)
 	{
 		// this creates a new object from the provided network
+
+		const schema = schemas[type]
 		await knex.transaction(async function(trx)
 		{
 			object.dbid = (await trx('item')
-				.returning('item.dbid')
+				.returning('dbid@item')
 				.insert({
-					net: net,
-					detail: 0,
+					'@net': netdbid,
+					'detail@$type': 0,
 					id: object.id,
-					type: type,
+					'$type': type,
 					cust: object.cust
 				}))[0]
-			await trx('item').insert({detail: object.dbid}).where('dbid', object.dbid)
-			if (type == 'net') {
-				await trx('net').insert({
-					orig: object.dbid,
-					time: object.time,
-					where: object.where,
-					config: object.config
-				})
-			} else if (type == 'user') {
-				await trx('user').insert({
-					orig: object.dbid,
-					time: object.time
-				})
-			} else if (type == 'prof') {
-				await trx('prof').insert({
-					orig: object.dbid,
-					time: object.time,
-					user: await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', 'user')
-						.andWhere('id', object.user),
-					attr: object.attr,
-					val: object.val
-				})
-			} else if (type == 'post') {
-				await trx('post').insert({
-					orig: object.dbid,
-					time: object.time,
-					user: await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', 'user')
-						.andWhere('id', object.user),
-					reply: object.reply && await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', 'post')
-						.andWhere('id', object.reply),
-					topic: object.topic && await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', 'topic')
-						.andWhere('id', object.topic)
-				})
-			} else if (type == 'topic') {
-				await trx('topic').insert({
-					orig: object.dbid,
-					time: object.time,
-					name: object.name
-				})
-			} else if (type == 'opin') {
-				await trx('opin').insert({
-					orig: object.dbid,
-					time: object.time,
-					user: await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', 'user')
-						.andWhere('id', object.user),
-					what: await trx('item')
-						.first('dbid')
-						.where('net', net)
-						.andWhere('type', object.type)
-						.andWhere('id', object.what)
-				})
-			}
+			await trx('item').update({'detail@$type': object.dbid}).where('dbid@item', object.dbid)
+			const details = {}
+			for (colnonref of schema.colsnonref)
+				if (colnonref in object)
+					details[colnonref] = object[colnonref]
+			for (colref of schema.colsref)
+				if (colref.name in object)
+					details[colref.name] = await trx('item')
+						.first('dbid@item')
+						.where('id', object[colref.name])
+						.andWhere('@net', netdbid)
+						.andWhere('$type', colref.ref)
+			await trx(type).insert(details)
 		})
 
 		return object.dbid
 	}
 
-	this.mirror = async function(type, net, orig, object)
+	this.mirror = async function(type, netdbid, origdbid, object)
 	{
-		object.dbid = (await trx('item')
+		object.dbid = (await knex('item')
 			.returning('item.dbid')
 			.insert({
-				net: net,
-				detail: orig,
+				'@net': netdbid,
+				'detail@$type': origdbid,
 				id: object.id,
-				type: type,
+				'$type': type,
 				cust: object.cust
 			}))[0]
 		return object.dbid
